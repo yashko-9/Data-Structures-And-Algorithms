4e40f698947695d1d5b395bcafa34e1e792ab2f0
yashko-9 <52626874+yashko-9@users.noreply.github.com>
2019-07-08 14:39:30 +0300

    Add files via upload

---
+++ Sorting Algorithms/Main.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <cstdlib> 
+#include <ctime> 
+
+using namespace std;
+
+void swap(int* a, int* b) {
+	int tmp;
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
+
+void swapII(int &a, int &b) {
+	int tmp;
+	tmp = a;
+	a = b;
+	b = tmp;
+}
+
+
+void selectionSort(int *array, int size) {
+	int  minIndex;
+	for (int i = 0; i < size - 1; i++) {
+		minIndex = i;
+		for (int j = i + 1; j < size; j++) {
+			if (array[j] < array[minIndex]) {
+				minIndex = j;
+			}
+		}
+		swap(array[i], array[minIndex]);
+	}
+}
+
+
+
+int main() {
+
+	int n = 10;
+	
+
+	int* a = new int[n];
+
+	srand((unsigned)time(0));
+
+	for (int i = 0; i < n; i++)
+	{
+		a[i] = rand() % 10 + 1;
+	}
+
+	for (int i = 0; i < n; i++)
+		cout << " " << a[i];
+	cout << endl;
+
+	selectionSort(a, n);
+	
+
+	
+	for (int i = 0; i < n; i++)
+		cout << " " << a[i];
+
+	system("pause");
+	return 0;
+}
+
